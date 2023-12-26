49995a1356a275694db01258e2e61008a4e9c931
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2023-12-26 12:54:32 +0530

    FIX: path WIP

--- Front-End/src/Components/Pages/ProductCard.jsx
+++ Front-End/src/Components/Pages/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { addToCart } from "../store/productSlice.js";
+import { addToCart } from "../../store/productSlice.js";
 import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
